c73cbdf7e30f35d9f6cda75ea285f2e75a59163c
Gabriela Herrera <gaby-herrera-dev@outlook.es>
2024-08-22 08:41:58 -0600

    Controlador Order

--- src/main/java/org/esfe/services/implementations/OrderService.java
+++ src/main/java/org/esfe/services/implementations/OrderService.java
@@ -1,6 +1,8 @@
 package org.esfe.services.implementations;
 
 import org.esfe.models.Order;
+import org.esfe.models.OrderItem;
+import org.esfe.models.User;
 import org.esfe.repository.IOrderRepository;
 import org.esfe.repository.IUserRepository;
 import org.esfe.services.interfaces.IOrderService;
@@ -14,6 +16,22 @@ import java.util.Optional;
 
 @Service
 public class OrderService implements IOrderService {
+    public Order createOrder(User user, List<OrderItem> orderItems) {
+        Order order = new Order();
+        order.setUser(user);
+        order.setOrderItems (orderItems);
+        order.setStatus("ACTIVE");
+
+        double totalAmount = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalAmount += orderItem.getPrice() * orderItem.getQuantity();
+        }
+
+        order.setTotalAmount(totalAmount);
+
+        return orderRepository.save(order);
+    }
+
 
     @Autowired
     private IOrderRepository orderRepository;
