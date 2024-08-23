6e6c5dae19dea469635909a6dd33cf5ba65d6008
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2024-08-22 18:52:26 -0600

    feat: :sparkles: improve loggin for test and add healthcheck endpoint

--- src/main/java/org/esfe/controllers/HomeController.java
+++ src/main/java/org/esfe/controllers/HomeController.java
@@ -3,6 +3,7 @@ package org.esfe.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping ("/")
@@ -12,4 +13,14 @@ public class HomeController {
         return "home/index";
     }
 
+    // controlador healthcheck
+    @GetMapping("/healthcheck")
+    @ResponseBody
+    public Object healthcheck(){
+        Object response = new Object(){
+            public String status = "ok";
+        };
+
+        return response;
+    }
 }
