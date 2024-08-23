1dc79eb4ca47364ab6c609043d2d0e130ed812a1
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2024-08-22 19:44:45 -0600

    fix: :bug: fix classa application

--- src/main/java/org/esfe/EcommerceApplication.java
+++ src/main/java/org/esfe/EcommerceApplication.java
@@ -2,10 +2,6 @@ package org.esfe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(scanBasePackages = "org.esfe")
 public class EcommerceApplication {
@@ -14,19 +10,5 @@ public class EcommerceApplication {
 		SpringApplication.run(EcommerceApplication.class, args);
 	}
 
-
-	@Configuration
-	public static class Myconfiguration{
-		@Bean
-		public WebMvcConfigurer corsConfigurer(){
-			return new WebMvcConfigurer() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					registry.addMapping("/**")
-							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-				}
-			};
-		}
-	}
 
 }
