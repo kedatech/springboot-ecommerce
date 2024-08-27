3dbec32c10d08b2cf9c548f0eb64900b1db1d5b8
Daniel Rodriguez <arevdevgames@gmail.com>
2024-08-26 19:44:23 -0600

    chore: change name User directory

--- src/main/java/org/esfe/security/SecurityConfig.java
+++ src/main/java/org/esfe/security/SecurityConfig.java
@@ -1,12 +1,9 @@
 package org.esfe.security;
 
-import org.esfe.models.User;
-import org.esfe.security.CustomAuthenticationEntryPoint;
 import org.esfe.services.implementations.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
