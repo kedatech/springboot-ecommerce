57148c861ee3652c8138e27b50706cde0f14d2a8
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2024-08-31 10:13:25 -0600

    feat: :sparkles: wompi services - get auth

--- src/main/java/org/esfe/services/interfaces/IWompiService.java
+++ src/main/java/org/esfe/services/interfaces/IWompiService.java
@@ -1,2 +1,8 @@
-package org.esfe.services.interfaces;public interface IWompiService {
+package org.esfe.services.interfaces;
+
+import org.esfe.models.dtos.wompi.PaymentLinkResponse;
+
+public interface IWompiService {
+    PaymentLinkResponse generateLink(Integer idEnlace, String urlEnlace);
 }
+
